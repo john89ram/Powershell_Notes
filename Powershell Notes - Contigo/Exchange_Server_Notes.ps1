d1271b70e71d1e7bff822ed78577382723cc42d5
john89ram <john89ram@gmail.com>
2022-08-30 10:17:55 -0500

    .

--- Powershell Notes - Contigo/Exchange_Server_Notes.ps1
+++ Powershell Notes - Contigo/Exchange_Server_Notes.ps1
@@ -12,3 +12,6 @@
     # To set MailboxFolderPermission:
         Set-MailboxFolderPermission -Identity ayla@contoso.com:\Marketing -User ed@contoso.com -AccessRights Owner
         # This will give Ed Ownership rights over Ayla's Marketing folder
+#----------------------------------*** Exchange Online Notes ***-----------------------------------------------------
+# All the following are still the same except EXO is added to the prefix
+    Get-EXOMailboxFolderPermission
